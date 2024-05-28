43e938fa87df72b694fc54ffe2933da1d4b35159
YoDe <153376007+Yodit-hub@users.noreply.github.com>
2024-05-28 23:20:20 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
   $('#demo').ripples({
-    resolution: 300,	//波紋が広がる速さ
-    dropRadius: 20,		//波紋の大きさ
-    perturbance: 0.01	//波紋の揺れの量
+    resolution: 300,
+    dropRadius: 20,
+    perturbance: 0.01
   });
+
+  setInterval(function() {
+    var $el = $('#demo');
+    var x = Math.random() * $el.outerWidth();
+    var y = Math.random() * $el.outerHeight();
+    var dropRadius = 20;
+    var strength = 0.04 + Math.random() * 0.04;
+
+    $el.ripples('drop', x, y, dropRadius, strength);
+  }, 400);
 });
-
-
