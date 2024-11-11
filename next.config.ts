15ff5626120b9f188fa80cf4563e71bec590111d
Yodit-hub <153376007+Yodit-hub@users.noreply.github.com>
2024-11-11 21:57:44 +0900

    first

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // GitHub Pages用の設定
+  basePath: '/Yodit-hub.github.io', // あなたのリポジトリ名を指定
+  images: {
+    unoptimized: true, // GitHub Pagesで画像を使用する場合必要
+  },
 };
 
 export default nextConfig;
