967a43232024d70a56d3a59eb818ea684ed2c474
Yodit-hub <153376007+Yodit-hub@users.noreply.github.com>
2024-11-24 03:32:10 +0900

    Update .gitignore and next.config.ts for build configuration
    - Updated .gitignore to exclude unnecessary files:
      - package-lock.json
      - directory_content_extractor.py
      - project_contents.txt
    - Updated next.config.ts to adjust basePath for GitHub Pages deployment:
      - Added logic to set basePath dynamically based on production or development environment.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,26 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  // GitHub Pages用の設定
-  basePath: '/Yodit-hub.github.io', // あなたのリポジトリ名を指定
+  basePath: isProd ? '/Yodit-hub.github.io' : '',
   images: {
     unoptimized: true, // GitHub Pagesで画像を使用する場合必要
   },
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: 'export',
+//   // GitHub Pages用の設定
+//   //basePath: '/Yodit-hub.github.io', // あなたのリポジトリ名を指定 ※開発サーバーでの確認時はbasePathの設定を一時的にコメントアウトする
+//   images: {
+//     unoptimized: true, // GitHub Pagesで画像を使用する場合必要
+//   },
+// };
+
+// export default nextConfig;
